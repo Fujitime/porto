5f23c81bf6fe3d2396be4875b630fe41b7d9b813
Fujitime <racun1601@gmail.com>
2025-03-19 10:52:56 +0700

    feat: thumbnail

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,6 +8,7 @@ const postsCollection = defineCollection({
     description: z.string(),
     isPublish: z.boolean(),
     isDraft: z.boolean().default(false),
+    image: z.string().optional(), 
   }),
 });
 
